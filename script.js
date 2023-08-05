3e062aa0b7016a1463afe7bed71ae95c9881df8c
danieldutra98 <104599671+danieldutra98@users.noreply.github.com>
2023-08-05 11:47:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 function calculateArbitrage() {
     const odds1 = parseFloat(document.getElementById('odds1').value);
+    const oddsDraw = parseFloat(document.getElementById('oddsDraw').value);
     const odds2 = parseFloat(document.getElementById('odds2').value);
-    const betAmount1 = parseFloat(document.getElementById('betAmount1').value);
 
-    if (odds1 <= 0 || odds2 <= 0 || betAmount1 <= 0) {
+    if (odds1 <= 0 || oddsDraw <= 0 || odds2 <= 0) {
         alert('Por favor, insira valores válidos.');
         return;
     }
 
-    const arbitrageAmount = (betAmount1 * odds1) / odds2;
+    const totalInvestment = 100; // Defina o valor total que você deseja investir (pode ser alterado conforme sua preferência)
+    const totalOdds = 1 / odds1 + 1 / oddsDraw + 1 / odds2;
+    const betAmount1 = (totalInvestment / (odds1 * totalOdds)).toFixed(2);
+    const betAmountDraw = (totalInvestment / (oddsDraw * totalOdds)).toFixed(2);
+    const betAmount2 = (totalInvestment / (odds2 * totalOdds)).toFixed(2);
 
-    document.getElementById('arbitrageAmount').textContent = arbitrageAmount.toFixed(2);
+    document.getElementById('betAmount1').textContent = betAmount1;
+    document.getElementById('betAmountDraw').textContent = betAmountDraw;
+    document.getElementById('betAmount2').textContent = betAmount2;
     document.getElementById('result').classList.remove('hidden');
 }
