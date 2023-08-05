49e15aa161bb99f667614c7b32ce5bfbde6aebea
danieldutra98 <104599671+danieldutra98@users.noreply.github.com>
2023-08-05 08:44:59 -0300

    Site das apostas

---
+++ script.js
@@ -0,0 +1,17 @@
+document.getElementById('arbitrageForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const odds1 = parseFloat(document.getElementById('odds1').value);
+    const odds2 = parseFloat(document.getElementById('odds2').value);
+    const betAmount1 = parseFloat(document.getElementById('betAmount1').value);
+
+    if (odds1 <= 0 || odds2 <= 0 || betAmount1 <= 0) {
+        alert('Por favor, insira valores válidos.');
+        return;
+    }
+
+    const arbitrageAmount = (betAmount1 * odds1) / odds2;
+
+    document.getElementById('arbitrageAmount').textContent = arbitrageAmount.toFixed(2);
+    document.getElementById('result').classList.remove('hidden');
+});
